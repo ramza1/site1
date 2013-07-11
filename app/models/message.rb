9ccf85d70ecb89145119396ef110c0b7701e5c4c
Uchenna Okafor <ramzauchenna@gmail.com>
2013-07-11 06:56:22 +0100

    fj

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,7 +10,7 @@ class Message
   attribute :priority # type: Integer, default: 0
   attribute :phone_number
 
-  #attr_accessible :name, :email, :content, :phone_number
+  attr_accessible :name, :email, :content, :phone_number
 
   validates_presence_of :name
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
