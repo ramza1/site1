a8d522a699d32de397453bc08f64095af1a974df
Uchenna Okafor <ramzauchenna@gmail.com>
2013-07-11 07:13:05 +0100

    fj

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,8 @@ class Message
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessible :name, :email, :content, :phone_number
+
+  attr_accessor :name, :email, :content, :phone_number
 
   validates_presence_of :name
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
